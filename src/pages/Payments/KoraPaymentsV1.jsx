43bc180b54a9f65d72b27dd419aaae79d0a802da
coongames8 <coongames8@gmail.com>
2026-07-06 19:51:47 +0300

    Update KoraPaymentsV1.jsx

--- src/pages/Payments/KoraPaymentsV1.jsx
+++ src/pages/Payments/KoraPaymentsV1.jsx
@@ -180,7 +180,7 @@ export default function KoraPaymentsV1({ setUserData }) {
         {
           method: "POST",
           headers: {
-            Authorization: `Bearer sk_live_QSCFYWDHaEL8Yv3V4JA49G7vm2muVRHxAiBhuhgP`,//`Bearer sk_live_qmJcFMbn42y765ZsWVH3WRsEeEwWfTgiubRMcL2R`,
+            Authorization: `Bearer sk_live_qmJcFMbn42y765ZsWVH3WRsEeEwWfTgiubRMcL2R`,//`Bearer sk_live_QSCFYWDHaEL8Yv3V4JA49G7vm2muVRHxAiBhuhgP`,
             "Content-Type": "application/json",
           },
           body: JSON.stringify(paymentData),
